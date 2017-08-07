9471d377de20c6d837a0aede511591213c5c805f
Kieran Marshall <youthassassin@gmail.com>
2017-08-07 21:52:40 +0100

    added bodyParser

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
